ccd041d35ebe3bd2b7e8461ba73c66984f9e7630
UmuTurk230315070 <230315070@ogr.cbu.edu.tr>
2026-04-07 22:17:59 +0300

    Refactor custom_equation to use keyword-only argument

--- Week04/functions_umut_turk.py
+++ Week04/functions_umut_turk.py
@@ -1,6 +1,6 @@
 custom_power = lambda x = 0 , /, e = 1: x**e
 
-def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int=  1, c: int = 1) -> float:
+def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int=  1, *, c: int = 1) -> float:
     """
     This function returns the result of an operation based on the specified base and exponent values.
 
